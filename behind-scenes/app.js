ba1e95528dc798d446a580182e98734db3e215dd
Julio <juliornellas@gmail.com>
2022-12-05 10:37:39 -0300

    behind the scenes

--- behind-scenes/app.js
+++ behind-scenes/app.js
@@ -10,12 +10,78 @@ const app = Vue.createApp({
       this.currentUserInput = event.target.value;
     },
     setText() {
-      this.message = this.currentUserInput;
+      // this.message = this.currentUserInput;
+      // console.dir(this.$refs.userText);
+      // console.log(this.$refs.userText.value);
+      this.message = this.$refs.userText.value;
     },
+  },
+
+  //Nothing on screen
+  //No access to data(), methods, etc
+  beforeCreate() {
+    console.log('beforeCreate()', this.message)
+  },
+
+  //Nothing on screen
+  //the $el property will not be available
+  //Data(), methods, etc are accessible and reactive
+  //Created to beforeMount -> compile template
+  created(){
+    console.log('created()', this.message)
+  },
+
+  //Nothing on screen
+  //Data(), methods, etc are accessible
+  //This hook can be used to access the DOM state before Vue updates the DOM. It is also safe to modify component state inside this hook.
+  //This hook is not called during server-side rendering.
+  beforeMount() {
+    console.log('beforeMount()', this.message)
+  },
+
+  //Data(), methods, etc are processed, template was compiled and all is visible on screen
+  //Useful when need access to the component rendered DOM
+  //This hook is not called during server-side rendering.
+  mounted() {
+    console.log('mounted()', this.message)
+  },
+
+  //The data was changed but on screen (DOM) still the oldest
+  //This hook is not called during server-side rendering.
+  beforeUpdate(){
+    console.log('beforeUpdate()', this.message)
+  },
+
+  //DOM load the new data
+  //This hook is not called during server-side rendering.
+  updated(){
+    console.log('update()', this.message)
+  },
+
+  //You'll still see the app in the DOM
+  // the component instance is still fully functional.
+  //This hook is not called during server-side rendering.
+  beforeUnmount() {
+    console.log('beforeUnmount()', this.message)
+  },
+
+  //Removed from the DOM
+  //Use this hook to clean up manually created side effects such as timers, DOM event listeners or server connections.
+  //This hook is not called during server-side rendering.
+  unmounted() {
+    console.log('unmounted()', this.message)
   },
 });
 
 app.mount('#app');
+
+setTimeout(
+  () => {
+    app.unmount();//Just to see how beforeUnmount() and unmounted() works
+  },
+  3000
+);
+
 
 /**
  * APP2
@@ -25,7 +91,10 @@ Vue.createApp({
     return {
       favoriteMeal: 'Hamburger'
     }
-  }
+  },
+  template: `
+    <p>{{favoriteMeal}}</p>
+  `
 })
 .mount('#app2')
 
@@ -67,5 +136,5 @@ proxy.message = 'New Hello';
 
 proxy.other = 'New other message';
 
-console.log('Message', proxy.message)
-console.log('Other', proxy.other)
+// console.log('Message', proxy.message)
+// console.log('Other', proxy.other)
